d7b3e7cd49d69fbc24c6fb9f708b93c29fc0daa4
Dale de Silva <daledesilva@gmail.com>
2022-01-16 22:23:35 +1100

    test await

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ miro.onReady(() => {
     extensionPoints: {
       
       bottomBar: {
-        title: 'convert mind map 6',
+        title: 'convert mind map 7',
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
@@ -28,7 +28,7 @@ miro.onReady(() => {
 async function startMindMapConversion() {
 
     const mindMap = await getMindMap();
-    createVerticalMindMap(mindMap);
+    await createVerticalMindMap(mindMap);
 
     // getMindMap().then( result => {
     //     console.log('result', result);
@@ -237,7 +237,7 @@ async function createVerticalMindMap(mindMap) {
 
     // mindMap.node
 
-    miro.board.widgets.create({
+    mindMap.newNode = miro.board.widgets.create({
         type: 'shape',
         text: mindMap.node.text,
         x: origin.x,
@@ -245,6 +245,8 @@ async function createVerticalMindMap(mindMap) {
         // width: sticker.bounds.width,
         // height: sticker.bounds.height,
     })
+
+    console.log('mindMap', mindMap);
 
     miro.showNotification('Mind map converted');
     
