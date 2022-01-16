ba3a57c48975e291dcbb3744800ea975802d0c92
Dale de Silva <daledesilva@gmail.com>
2022-01-16 23:10:41 +1100

    increase version

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ miro.onReady(() => {
     extensionPoints: {
       
       bottomBar: {
-        title: 'convert mind map 10',
+        title: 'convert mind map 11',
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
