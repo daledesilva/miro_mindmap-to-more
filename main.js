eb16604dfe92501d49648066d3c009b093c8a5c9
Dale de Silva <daledesilva@gmail.com>
2022-01-16 22:14:46 +1100

    increase iterator log

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ miro.onReady(() => {
     extensionPoints: {
       
       bottomBar: {
-        title: 'convert mind map 2',
+        title: 'convert mind map 3',
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
