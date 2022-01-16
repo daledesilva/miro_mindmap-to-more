400d56c6c78826869ac2e1dc1ea98a73d234bcfe
Dale de Silva <daledesilva@gmail.com>
2022-01-17 00:30:13 +1100

    layout

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ miro.onReady(() => {
     extensionPoints: {
       
       bottomBar: {
-        title: 'convert mind map 9',
+        title: 'convert mind map 1',
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
@@ -299,7 +299,7 @@ async function createChildrenAbove(parentNode) {
             // height: sticker.bounds.height,
         })
         childNode.newRef = newRefs[0];
-        await createChildrenBelow(childNode);
+        await createChildrenAbove(childNode);
     })
 
 }
