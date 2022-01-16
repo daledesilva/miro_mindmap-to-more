8396e7ab0387cad8f1f4f0764c274df479b10070
Dale de Silva <daledesilva@gmail.com>
2022-01-16 22:20:56 +1100

    simplify async

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ miro.onReady(() => {
     extensionPoints: {
       
       bottomBar: {
-        title: 'convert mind map 5',
+        title: 'convert mind map 6',
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
@@ -27,13 +27,13 @@ miro.onReady(() => {
 
 async function startMindMapConversion() {
 
-    // const mindMap = await getMindMap();
-    // await createVerticalMindMap(mindMap);
+    const mindMap = await getMindMap();
+    createVerticalMindMap(mindMap);
 
-    getMindMap().then( result => {
-        console.log('result', result);
-        createVerticalMindMap(result);
-    })
+    // getMindMap().then( result => {
+    //     console.log('result', result);
+    //     createVerticalMindMap(result);
+    // })
 }
 
 
@@ -232,7 +232,7 @@ async function createVerticalMindMap(mindMap) {
 
     const origin = {
         x: mindMap.node.bounds.x,
-        y: mindMap.node.bounds.y+200,
+        y: mindMap.node.bounds.y+1000,
     }
 
     // mindMap.node
