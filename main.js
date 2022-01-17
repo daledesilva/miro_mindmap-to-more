ce970a3fd201c533ceccb9716b55151eb2269c62
Dale de Silva <daledesilva@gmail.com>
2022-01-18 00:04:08 +1100

    tweak

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ miro.onReady(() => {
     extensionPoints: {
       
       bottomBar: {
-        title: 'convert mind map 5',
+        title: 'convert mind map 6',
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
@@ -33,6 +33,7 @@ async function startMindMapConversion() {
 
     const mindMap = await getMindMap();
     await createVerticalMindMap(mindMap);
+    console.log('BETWEEN');
     await refineDownwardBranchLayout(mindMap);
 
     miro.showNotification('Mind map converted');
