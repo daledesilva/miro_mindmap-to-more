0d7793253c1cdd690d82cdaafa89532bdcc76900
Dale de Silva <daledesilva@gmail.com>
2022-01-18 00:15:46 +1100

    clone

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ miro.onReady(() => {
     extensionPoints: {
       
       bottomBar: {
-        title: 'convert mind map 7',
+        title: 'convert mind map 8',
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
