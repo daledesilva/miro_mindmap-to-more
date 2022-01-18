4d2ebd14f11b3ec27dec61ab21b040a8962f11dd
Dale de Silva <daledesilva@gmail.com>
2022-01-18 13:18:48 +1100

    logs for testing

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ miro.onReady(() => {
     extensionPoints: {
       
       bottomBar: {
-        title: 'convert mind map 6',
+        title: 'convert mind map 7',
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
@@ -370,7 +370,6 @@ async function sizeNodeAndLayOutItsChildren(parentNode) {
 
     // It's got children, so calculate them first to get the overall width
     let thisTreeWidth = 0;
-    console.log('parentNode', parentNode);
     for( let k=0; k<childNodes.length; k++ ) {
         const childNode = childNodes[k];
         const childTreeWidth = await sizeNodeAndLayOutItsChildren( childNode );
@@ -398,6 +397,12 @@ async function sizeNodeAndLayOutItsChildren(parentNode) {
         
         const childTopEdge = childNode.newRef.bounds.y - childNode.newRef.bounds.height/2; // This width might not be right - It should be top if it's rotated, plus the ref's not been updated since adjusting
         const offsetY = (parentBottomEdge+VERT_BUFFER) - childTopEdge;
+
+        console.log('parentBottomEdge', parentBottomEdge);
+        console.log('VERT_BUFFER', VERT_BUFFER);
+        console.log('childTopEdge', childTopEdge);
+        console.log('offsetY', offsetY);
+        console.log('childNode.newRef.y', childNode.newRef.y);
 
         childNode.newRef.x += offsetX;
         childNode.newRef.y += offsetY;
