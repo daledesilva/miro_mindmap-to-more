809fd38a17b2b76bd1a6c168dc0ddbcc9aa18df0
Dale de Silva <daledesilva@gmail.com>
2022-01-18 16:15:21 +1100

    typo

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ miro.onReady(() => {
     extensionPoints: {
       
       bottomBar: {
-        title: 'convert mind map 9',
+        title: 'convert mind map 10',
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
@@ -448,7 +448,7 @@ function getNodeTreeAsArray(node) {
     const nodeArr = [node];
 
     const childNodes = node.childNodes || node.childNodesAfter.concat(node.childNodesBefore);
-    for(node in childNodes) {
+    for(childNode in childNodes) {
         nodeArr = nodeArr.concat( getNodeTreeAsArray(childNode) );
     }
 
